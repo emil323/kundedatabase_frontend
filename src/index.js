d66cf93af4125238d6b0920af2f0663fb4501451
Kristoffer <skyggelaus@gmail.com>
2019-01-19 11:48:12 +0100

    Installed JQuery, Popper and Bootstrap. Added a button to test Bootstap compatability with React

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Component} from 'react';
+import Bootstrap from 'bootstrap';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Button extends Component {
+    render() {
+        return (
+            <div>
+                <button type="button" className="btn btn-primary">Button</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Button />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
