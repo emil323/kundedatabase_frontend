05e6a0e85eb7631284a27f116755df6e7b8a7841
emil323 <emilkalst@gmail.com>
2019-01-21 20:57:34 +0100

    react-hamburger-menu implemented

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap'
+import { Collapse, Nav, Navbar, NavbarBrand} from 'reactstrap'
+import Hamburger from 'react-hamburger-menu'
 import Logo from '../../assets/Img/ecit-logo.png'
 import './Header.css'
 import Menu from './../Menu/Menu'
@@ -8,24 +9,34 @@ export default class Header extends React.Component {
     constructor(props) {
       super(props);
   
-      this.toggleNavbar = this.toggleNavbar.bind(this);
+      this.toggleHamburger = this.toggleHamburger.bind(this);
       this.state = {
-        collapsed: true
+        open: false
       };
     }
   
-    toggleNavbar() {
+    toggleHamburger() {
       this.setState({
-        collapsed: !this.state.collapsed
+        open: !this.state.open
       })
     }
     render() {
       return (
         <div>
           <Navbar color="dark" dark expand="md">
+            <div class="hidden-lg hidden-md hidden-sm">
+              <Hamburger
+                isOpen={this.state.open}
+                menuClicked={this.toggleHamburger}
+                width={30}
+                height={20}
+                strokeWidth={3}
+                animationDuration={0.3}
+              />
+            </div>
             <NavbarBrand href="/" className="mx-auto"><img alt="Logo" src={Logo} /></NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse isOpen={!this.state.collapsed} navbar className="hidden-lg">
+          
+            <Collapse isOpen={this.state.open} navbar className="hidden-lg">
               <Nav navbar className="hidden-lg hidden-md hidden-sm">
                 <Menu/>
               </Nav>
