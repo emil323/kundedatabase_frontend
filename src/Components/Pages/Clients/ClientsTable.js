2749b6d4118d76485179335102031acec062a969
Kristoffer <skyggelaus@gmail.com>
2019-02-04 19:53:13 +0100

    Added a drowndown-button component

--- src/Components/Pages/Clients/ClientsTable.js
+++ src/Components/Pages/Clients/ClientsTable.js
@@ -1,20 +1,38 @@
 import React from 'react'
+import DropdownBtn from '../../DropdownBtn/DropdownBtn';
 
 
- const ClientsTable = ({clients, deleteClient}) => {
+class ClientsTable extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            btnOptions: [
+                { tekst: 'Behandle', isHeader: 1, key: 1 },
+                { tekst: 'Vis', isHeader: 0, key: 2 },
+                { tekst: 'Slett', isHeader: 0, key: 3 },
+                { tekst: 'Placeholder', isHeader: 1, key: 4 },
+                { tekst: 'Placeholder', isHeader: 0, key: 5, },
+            ]
+        }
 
-        return(
+    }
+
+    render() {
+       
+        return (
             <tbody>
                 <tr>
-                    <th>{clients.id}</th>
-                    <td>{clients.firmanavn}</td>
-                    <td>{clients.kontaktperson}</td>
-                    <td>{clients.sistendret}</td>
-                    <td><button key={clients.id} onClick={() => {deleteClient(clients.id)}}>DEL</button></td>
+                    <th>{this.props.clients.id}</th>
+                    <td>{this.props.clients.firmanavn}</td>
+                    <td>{this.props.clients.kontaktperson}</td>
+                    <td>{this.props.clients.sistendret}</td>
+                    {/* <td><button key={this.props.clients.id} onClick={() => { this.props.deleteClient(this.props.clients.id) }}>DEL</button></td> */}
+                    <DropdownBtn options={this.state.btnOptions} />
                 </tr>
             </tbody>
         )
+    }
 }
 
 
