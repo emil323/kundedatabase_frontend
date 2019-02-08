1644d14d255c982e7074d1925a7f5a30526657b2
HelgesenJan <33288663+HelgesenJan@users.noreply.github.com>
2019-02-08 13:16:15 +0100

    Fixed upload styling and general function stuff

--- src/Components/ModalComponent/ModalComponent.js
+++ src/Components/ModalComponent/ModalComponent.js
@@ -6,73 +6,104 @@ import classNames from 'classnames'
 import API from '../../API/API';
 import { connect } from "react-redux";
 import {fetchFilesData} from '../../Store/Actions/filesActions'
+import './ModalComponent.css'
+
+import downloadIcon from "../../Assets/Icons/download.png"
 
 class ModalComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      files_to_upload: []
     };
 
-    this.toggle = this.toggle.bind(this);
-    
+    this.toggle = this.toggle.bind(this)
   }
 
   toggle() {
 
     this.setState(prevState => ({
       ...this.state,
-      modal: !prevState.modal
+      modal: !prevState.modal,
+      files_to_upload : []
     }));
 
   }
+
+
 
   onDrop(files) {
 
     files.forEach((file) => {
       const formData = new FormData();
       formData.append("file", file)
-      console.log(this.props)
-       API.files().folder(this.props.selected_folder).upload(formData)
+
+      this.setState((prevState) => ({
+       prevState , 
+        files_to_upload: [
+          ...prevState.files_to_upload,
+          formData
+        ]
+      }))
+
+      /* API.files().folder(this.props.selected_folder).upload(formData)
         .then((response) => {
           this.props.fetchFilesData(this.props.client_id, this.props.selected_folder)
         })
         .catch((err) => {
           console.log(err)
         })
-        
+        */
     })
   }
 
-  
+ upload() {
+   const formData =new FormData()
+   this.state.files_to_upload.forEach(file => {
+     formData.append(file.get('file'))
+   })
+   API.files().folder(this.props.selected_folder).upload(formData)
+        .then((response) => {
+          this.props.fetchFilesData(this.props.client_id, this.props.selected_folder)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+ }
 
   render() {
+    
     return (
       <div className="container">
+      
         <Button color="primary" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+        <Modal centered isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>{this.props.selected_folder}</ModalHeader>
           <ModalBody>
           <Dropzone onDrop={this.onDrop.bind(this)}>
-          {({getRootProps, getInputProps, isDragActive}) => {
-            return (
-              <div
-                {...getRootProps()}
-                className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
-              >
-                <input {...getInputProps()} />
-                {
-                  isDragActive ?
-                    <p>Drop files here...</p> :
-                    <p>sssssssssssssssssssssssssssssssssssssssssss</p>
-                }
-              </div>
-            )
-          }}
-      </Dropzone>
+            {({getRootProps, getInputProps, isDragActive}) => {
+              return (
+                <div
+                  {...getRootProps()}
+                  className={classNames('dropzone',{'dropzone--isActive': isDragActive})}
+                >
+                  <img className="modalImage" src={downloadIcon}></img>
+                  <input  {...getInputProps()} />
+                  {
+                   <p className="modalP">{isDragActive ? "Slipp her..." : " Dra filer hit..."}</p>
+                  }
+            
+                  <Button  color="primary" className="modalBtn">Eller velg filer </Button>
+                  <div className="modalDiv">{this.state.files_to_upload.map((file) => {return (<p>{file.get('file').name}</p>)})}</div>
+                </div>
+              )
+            }}  
+          </Dropzone>
+   
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="primary" onClick={this.toggle}>Last opp</Button>{' '}
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
