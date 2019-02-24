68e9295d48e9e53989ef6207b628f6f96b6e0f0f
Jan Helge <villy9999@hotmail.com>
2019-02-24 21:39:40 +0100

    Changed back to hearts

--- src/Components/Pages/Clients/ClientData.js
+++ src/Components/Pages/Clients/ClientData.js
@@ -44,8 +44,8 @@ class ClientsData extends React.Component {
                     <td> 
                         <label for="id-of-input" class="custom-checkbox">
                             <input  type="checkbox" id="id-of-input"/>
-                            <i class="glyphicon glyphicon-star-empty"></i>
-                            <i onClick={this.updateFavourites} class="glyphicon glyphicon-star"></i>
+                            <i class="glyphicon glyphicon-heart-empty"></i>
+                            <i onClick={this.updateFavourites} class="glyphicon glyphicon-heart"></i>
                         </label>
                     </td>
                 </tr>
