5009f4e247883879826e215e4ae38efca0fd520c
HelgesenJan <33288663+HelgesenJan@users.noreply.github.com>
2019-03-01 11:24:43 +0100

    Jumbotron name and date

--- src/Components/Jumbotron/Welcome.js
+++ src/Components/Jumbotron/Welcome.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 import './Jumbotron.css';
+import { authContext } from './../../Auth/adalConfig';
 
 
-const Welcome = (props) => {
-    return (
-        <div>
-            <Jumbotron className="Jumbotron-Home">
-                    <h1 className="display=3">Velkommmen!</h1>
-                    <p className="lead">Velkomstmelding</p>
-                    <hr className="my-2" />
-                    <p>Brødtekst</p>
-                    <p className="lead">
-                        <Button color="info">Knapp</Button>
-                    </p>
-            </Jumbotron>
 
-        </div>
-    )
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: null,
+            name: null
+        }
+    }
+    render(){
+        var curDate = Date();
+        return (
+            <div>
+                <Jumbotron className="Jumbotron-Home">
+                        <h1 className="display=3">Velkommmen {this.state.name}!</h1>
+                        <p className="lead">{curDate}</p>
+                        <hr className="my-2" />
+                      {/*   <p>Brødtekst</p>
+                        <p className="lead">
+                            <Button color="info">Knapp</Button>
+                        </p> */}
+                </Jumbotron>
+    
+            </div>
+        )
+    }
+    componentDidMount() {
+        this.setState({email: authContext.getCachedUser().userName,
+                       name: authContext.getCachedUser().profile.name })
+    }
 }
 
-export default Welcome;
+
+export default (Welcome);
