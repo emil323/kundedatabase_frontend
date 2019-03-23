a69955e86ee07413a8eabaa6d9f66818893015d8
emil323 <emilkalst@gmail.com>
2019-03-23 10:51:18 +0100

    callback url to not include pathname

--- src/Settings.js
+++ src/Settings.js
@@ -1,5 +1,5 @@
 
 
  export const API_URL = 'https://kundedbapi.azurewebsites.net/' //https://kundedbapi.azurewebsites.net/'
- export const CALLBACK_URL = window.location.href
+ export const CALLBACK_URL = window.location.protocol + '//' + window.location.host
  export const ADMIN_ROLE = 'Admin'
