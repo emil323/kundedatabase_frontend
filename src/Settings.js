d681d41fd89810001d6338b0ed4172f3c2842a99
emil323 <emilkalst@gmail.com>
2019-03-25 15:04:56 +0100

    new api

--- src/Settings.js
+++ src/Settings.js
@@ -2,11 +2,11 @@
 /*
 *
 * For using live API
-*    https://kundedbapi.azurewebsites.net/ 
+*    https://dpkdapi.azurewebsites.net
 * For using API running locally
 *    http://localhost:8080
 *
 */
- export const API_URL = 'https://kundedbapi.azurewebsites.net/' 
+ export const API_URL = 'https://dpkdapi.azurewebsites.net' 
  export const CALLBACK_URL = window.location.protocol + '//' + window.location.host
  export const ADMIN_ROLE = 'Admin'
