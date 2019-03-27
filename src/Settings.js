c76103be67fc16eafe7b17313d63a6eb66aad8f4
emil323 <emilkalst@gmail.com>
2019-03-27 19:52:05 +0100

    api url

--- src/Settings.js
+++ src/Settings.js
@@ -9,6 +9,6 @@
 */
  export const APP_ID = '36bdb668-3a4e-496d-bb39-b327fcfcaa32'
  export const TENANT_ID = 'bc758dd0-ab53-4372-9a7c-e98a9620862c'
- export const API_URL = 'http://localhost:8080' 
+ export const API_URL = 'https://dpkdapi.azurewebsites.net' 
  export const CALLBACK_URL = window.location.protocol + '//' + window.location.host
  export const ADMIN_ROLE = 'Admin'
